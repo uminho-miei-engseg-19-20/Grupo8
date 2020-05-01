f549db44d0c8cc131d10471be08267957030e4af
Utilizador do sistema <user@CSI.EngSeg>
2020-05-01 23:42:46 +0100

    Resoluçao da aula10

---
+++ Aula10/under_vuln.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int MAX_SIZE = 2048;
+
+
+void vulneravel (char *origem, size_t tamanho) {
+        size_t tamanho_real;
+        char *destino;
+        if (tamanho < MAX_SIZE) {
+				
+                tamanho_real = tamanho - 1; // Não copiar \0 de origem para destino
+                destino = (char *) malloc(tamanho_real);
+				
+                
+        }
+		else{ printf("tamanho pequeno\n");}
+}
+
+int main() {
+		char *o = (char *) malloc(sizeof(char));
+		
+        vulneravel(o,0);
+}
